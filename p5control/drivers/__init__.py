3fb3238b043ce9dd0fea3972e94968331e749b69
schmampf@VNA <oliver.irtenkauf@uni-konstanz.de>
2023-01-24 10:52:54 +0100

    include vna and thermo in driver.__init__

--- p5control/drivers/__init__.py
+++ p5control/drivers/__init__.py
@@ -8,6 +8,8 @@ Device drivers
 """
 from .keysight34461A import Keysight34461A
 from .keysightB2962A import KeysightB2962A
+from .GIR2002 import GIR2002
+from .ZNB40 import ZNB40
 
 """
 Test drivers
