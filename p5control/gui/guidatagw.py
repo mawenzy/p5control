bf20a53dfa30dc3a631e5c12b99077f455ca49f4
bluefors <39279779+mawenzy@users.noreply.github.com>
2023-01-27 17:40:57 +0100

    added wrapping around netrefs

--- p5control/gui/guidatagw.py
+++ p5control/gui/guidatagw.py
@@ -2,9 +2,68 @@
 Wrap DataGateway to open Dialog if Connection errors occur, such that
 the gui can assume the dataserver to always answer 
 """
+from rpyc.core.netref import NetrefClass, BaseNetref, LOCAL_ATTRS
+from rpyc.core.protocol import Connection
+from rpyc.utils.classic import obtain
 from qtpy.QtWidgets import QMessageBox
 
 from ..gateway import DataGateway
+
+class WrapNetref():
+    def __init__(self, netref) -> None:
+        self._secret_netref = netref
+
+    def __str__(self) -> str:
+        return f"<p5control.gui.guidatagw.WrapNetref wrapping {self._secret_netref}>"
+
+    def __call__(self, *args, **kwargs):
+        try:
+            res = self._secret_netref(*args, **kwargs)
+            # print(f"WrapNetref call res {res}, {type(res)}")
+            if isinstance(res, BaseNetref):
+                return WrapNetref(res)
+            return res
+        except Exception as e:
+            print(f"__call__ exc {type(e)} caught: {e}")
+            raise
+
+    def __getattr__(
+        self,
+        attr: str
+    ):
+        # print(f"__getattr__ {attr}")
+        try:
+            res = getattr(self._secret_netref, attr)
+
+            if isinstance(res, BaseNetref):
+                return WrapNetref(res)
+            return res
+        except Exception as e:
+            print(f"__getattr__ {attr} exc {type(e)} caught: {e}")
+            raise
+
+    def __iter__(self):
+        res = getattr(self._secret_netref, '__iter__')()
+        # print(f"WrapNetref iter res {res}, {type(res)}")
+
+        if isinstance(res, BaseNetref):
+            return WrapNetref(res)
+        return res
+
+    def __next__(self):
+        res = getattr(self._secret_netref, '__next__')()
+        # print(f"WrapNetref next res {res}, {type(res)}")
+
+        if isinstance(res, BaseNetref):
+            return WrapNetref(res)
+        return res
+
+    @property
+    def __class__(self):
+        print("getting class")
+        res = getattr(self._secret_netref, '__class__')
+        return res
+
 
 class GuiDataGateway(DataGateway):
     """Wrapper for DataGateway to use in a GUI application.
@@ -22,7 +81,14 @@ class GuiDataGateway(DataGateway):
         """Allow shorthand gateway.attribute for gateway.root.attribute"""
         if self._connection:
             try:
-                return getattr(self._connection.root, attr)
+                res = getattr(self._connection.root, attr)
+                
+                if isinstance(res, BaseNetref):
+                    print(f"req: {attr}, wrapping return type")
+                    return WrapNetref(res)
+                else:
+                    return res
+
             except EOFError as e:
                 # make sure the connection is closed
                 self.disconnect()
