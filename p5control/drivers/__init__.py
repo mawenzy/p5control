a092a83f3d28a6424f158f3bac829efcf9e07c25
bluefors <61253184+schmampf@users.noreply.github.com>
2024-02-19 15:43:34 +0100

    remove custom made drivers from this version

--- p5control/drivers/__init__.py
+++ p5control/drivers/__init__.py
@@ -4,17 +4,6 @@ Base driver
 from .basedriver import BaseDriver
 
 """
-Device drivers
-"""
-from .adwingold2 import ADwinGold2
-from .adwingold2_v2 import ADDAwinGold2_v2
-from .keysight34461A import Keysight34461A
-from .keysightB2962A import KeysightB2962A
-from .gir2002 import GIR2002
-from .znb40 import ZNB40
-from .blueforsapi import BlueForsAPI
-
-"""
 Test driver
 """
 from .exampleinst import ExampleInst
