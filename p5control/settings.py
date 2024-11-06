61ce6c7908e4e7b121638b8dd2b14d9dc379244a
bluefors <61253184+schmampf@users.noreply.github.com>
2024-11-06 14:04:38 +0100

    more callback Threads. New GUI needs more callbacks

--- p5control/settings.py
+++ p5control/settings.py
@@ -15,7 +15,7 @@ MEASUREMENT_BASE_PATH = "/measurement"
 STATUS_MEASUREMENT_BASE_PATH = "/status"
 """path in the hdf5 file under which the statuses are stored"""
 
-CALLBACK_THREAD_COUNT = 5
+CALLBACK_THREAD_COUNT = 10
 """The amount of threads in the thread pool responsible for callbacks."""
 
 GATEWAY_ADDRESS = "localhost"
